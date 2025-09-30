c8bc07e18cc64df76bd4b42a2d304c488009cd2a
Maurya250 <aniketmaurya.dev@gmail.com>
2025-10-01 00:35:54 +0530

    testfile change

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,6 @@ def test_hello():
     client = app.test_client()
     res = client.get('/')
     assert res.status_code == 200
-    assert b"Hello DevOps" in res.data
+    assert b'Aniket' in res.data
+    assert b'DevOps' in res.data
+    assert b'Cybersecurity' in res.data
