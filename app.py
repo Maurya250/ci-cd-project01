b9d07f0d1fb840d5915776402b7b6d47c4af4013
Maurya250 <aniketmaurya.dev@gmail.com>
2025-09-30 23:04:34 +0530

    Initial commit: Hello DevOps project

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "i am DevOps enginner"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
