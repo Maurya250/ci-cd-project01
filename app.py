ed8fe3ded0831383718646142f970acf16f07dca
Maurya250 <aniketmaurya.dev@gmail.com>
2025-09-30 23:58:12 +0530

    app.py Changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "i am DevOps enginner"
+    return "Hello DevOps"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
